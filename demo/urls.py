1f67a1015bfc6c2b15cead01213201c9b7c56a9e
lukedoma <70923245+lukedoma@users.noreply.github.com>
2020-12-15 02:36:41 +1200

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+#     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('core.urls', namespace='core'))
 ]
 
